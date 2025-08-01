df3de077345e9a580edc28816523bc4180e74f9a
bagus farouk <b46usf@gmail.com>
2025-08-02 06:57:22 +0700

    commit

--- asset/js/game.js
+++ asset/js/game.js
@@ -119,7 +119,7 @@ window.addEventListener("DOMContentLoaded", () => {
     shootBtn.addEventListener("click", () => {
       if (!isKicked && player) {
         isKicked = true;
-        velocity.set(0, 10, -0.4);
+        velocity.set(0, 0.100, -0.4);
         player.scale.y = 2.8;
         setTimeout(() => player.scale.y = 3, 100);
 
