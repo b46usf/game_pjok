3a1e9a0cc11d804070916c94b0d8ad9053ae11d0
bagus farouk <b46usf@gmail.com>
2025-08-02 06:11:53 +0700

    commit

--- asset/js/game.js
+++ asset/js/game.js
@@ -96,11 +96,11 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   const label3 = createLabel("3", "#c0392b");
-  label3.position.set(10, 15, -25);
+  label3.position.set(10, 12, -22);
   scene.add(label3);
 
   const label4 = createLabel("4", "#2980b9");
-  label4.position.set(-10, 15, -25);
+  label4.position.set(-10, 12, -22);
   scene.add(label4);
 
   // === Kontrol Keyboard (Gerak horizontal sebelum ditendang) ===
