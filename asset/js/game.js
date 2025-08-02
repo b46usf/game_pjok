ff968c2cf10b6d4c0a19343582f67ca0f0703897
bagus farouk <b46usf@gmail.com>
2025-08-03 00:24:05 +0700

    commit

--- asset/js/game.js
+++ asset/js/game.js
@@ -21,7 +21,7 @@ const questions = {
   1: { soal: "Angka Genap", jawaban: ["2", "3"], benar: "2" },
   2: { soal: "Bilangan Prima", jawaban: ["4", "5"], benar: "5" },
   3: { soal: "Kelipatan 3", jawaban: ["6", "7"], benar: "6" },
-  4: { soal: "Lebih besar dari 5", jawaban: ["4", "6"], benar: "6" },
+  4: { soal: "Lebih besar dari 5", jawaban: ["4", "7"], benar: "7" },
   5: { soal: "Kurang dari 10", jawaban: ["12", "8"], benar: "8" }
 };
 
