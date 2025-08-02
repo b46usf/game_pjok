1d82395313156a0f39ce036cb1fa8071a4b3187a
bagus farouk <b46usf@gmail.com>
2025-08-02 23:24:46 +0700

    commit

--- asset/js/game.js
+++ asset/js/game.js
@@ -305,7 +305,7 @@ function updateLabelTextures() {
 }
 
 function createBall() {
-  const geometry = new THREE.SphereGeometry(0.2);
+  const geometry = new THREE.SphereGeometry(0.3, 32, 32);
   const texture = new THREE.TextureLoader().load("asset/image/soccer-ball.png");
   const material = new THREE.MeshStandardMaterial({ map: texture });
 
